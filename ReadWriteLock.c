29ce544c80731b913a81c6e970757941308ddecb
Mateusz Sulimowicz <mateusz.sulimowicz@syncron.com>
2021-12-27 23:57:15 +0100

    handle errors in RWLock

--- ReadWriteLock.c
+++ ReadWriteLock.c
@@ -15,29 +15,35 @@ struct RWLock {
 
 RWLock *rwlock_new() {
     RWLock *r = malloc(sizeof(RWLock));
+
     if (!r) {
         return NULL;
     }
 
-    // TODO: check retvals
-    pthread_mutex_init(&r->mutex, 0);
-    pthread_cond_init(&r->to_write, 0);
-    pthread_cond_init(&r->to_read, 0);
+    if (pthread_mutex_init(&r->mutex, 0) != 0) return NULL;
+
+    if (pthread_cond_init(&r->to_write, 0) != 0) {
+        pthread_mutex_destroy(&r->mutex);
+        return NULL;
+    }
+
+    if (pthread_cond_init(&r->to_read, 0) != 0) {
+        pthread_mutex_destroy(&r->mutex);
+        pthread_cond_destroy(&r->to_read);
+        return NULL;
+    }
     return r;
 }
 
 int rwlock_rd_lock(RWLock *lock) {
     int err;
-    err = pthread_mutex_lock(&lock->mutex);
-
-    if (err != 0) return err;
+    if ((err = pthread_mutex_lock(&lock->mutex)) != 0) return err;
 
     ++lock->wait_rd;
     if (lock->wait_wr > 0 || lock->work_wr > 0) { // TODO: check
         // reader should wait
         do {
-            err = pthread_cond_wait(&lock->to_read, &lock->mutex);
-            if (err != 0) return err;
+            if ((err = pthread_cond_wait(&lock->to_read, &lock->mutex)) != 0) return err;
         } while (lock->work_wr > 0);
     }
     --lock->wait_rd;
@@ -48,33 +54,26 @@ int rwlock_rd_lock(RWLock *lock) {
 
 int rwlock_rd_unlock(RWLock *lock) {
     int err;
-    err = pthread_mutex_lock(&lock->mutex);
-
-    if (err != 0) return err;
+    if ((err = pthread_mutex_lock(&lock->mutex)) != 0) return err;
 
     --lock->work_rd;
     if (lock->work_rd == 0 && lock->wait_wr > 0) {
-        err = pthread_cond_signal(&lock->to_write);
+        if ((err = pthread_cond_signal(&lock->to_write)) != 0) return err;
     } else if (lock->work_rd == 0 && lock->wait_rd > 0) {
-        err = pthread_cond_broadcast(&lock->to_read);
+        if ((err = pthread_cond_broadcast(&lock->to_read)) != 0) return err;
     }
-
-    if (err != 0) return err;
 
     return pthread_mutex_unlock(&lock->mutex);
 }
 
 int rwlock_wr_lock(RWLock *lock) {
     int err;
-    err = pthread_mutex_lock(&lock->mutex);
-
-    if (err != 0) return err;
+    if ((err = pthread_mutex_lock(&lock->mutex)) != 0) return err;
 
     ++lock->wait_wr;
     while (lock->work_rd > 0 || lock->work_wr > 0) {
         // writer should wait
-        err = pthread_cond_wait(&lock->to_write, &lock->mutex);
-        if (err != 0) return err;
+        if ((err = pthread_cond_wait(&lock->to_write, &lock->mutex)) != 0) return err;
     }
     --lock->wait_wr;
 
@@ -84,19 +83,15 @@ int rwlock_wr_lock(RWLock *lock) {
 
 int rwlock_wr_unlock(RWLock *lock) {
     int err;
-    err = pthread_mutex_lock(&lock->mutex);
-
-    if (err != 0) return err;
+    if ((err = pthread_mutex_lock(&lock->mutex)) != 0) return err;
 
     --lock->work_wr;
     // always at most only one writer working
     if (lock->wait_rd > 0) {
-        err = pthread_cond_broadcast(&lock->to_read);
+        if ((err = pthread_cond_broadcast(&lock->to_read)) != 0) return err;
     } else if (lock->wait_wr > 0) {
-        err = pthread_cond_signal(&lock->to_write);
+        if ((err = pthread_cond_signal(&lock->to_write)) != 0) return err;
     }
-
-    if (err != 0) return err;
 
     return pthread_mutex_unlock(&lock->mutex);
 }
