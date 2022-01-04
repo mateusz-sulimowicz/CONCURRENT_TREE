e1a9323654d07d667e7e9eefb13b9f126fda54e0
mateusz <ms429603@students.mimuw.edu.pl>
2022-01-04 20:24:39 +0100

    Add comments

--- ReadWriteLock.c
+++ ReadWriteLock.c
@@ -26,15 +26,9 @@ RWLock *rwlock_new() {
         syserr("", EMEMORY);
     }
 
-    if ((err = pthread_mutex_init(&r->mutex, 0)) != 0) {
-        syserr("", err);
-    }
-    if (pthread_cond_init(&r->to_write, 0) != 0) {
-        syserr("", err);
-    }
-    if (pthread_cond_init(&r->to_read, 0) != 0) {
-        syserr("", err);
-    }
+    if ((err = pthread_mutex_init(&r->mutex, 0)) != 0) syserr("", err);
+    if (pthread_cond_init(&r->to_write, 0) != 0) syserr("", err);
+    if (pthread_cond_init(&r->to_read, 0) != 0) syserr("", err);
 
     r->wait_wr = 0;
     r->wait_rd = 0;
@@ -46,17 +40,14 @@ RWLock *rwlock_new() {
 // Acquire read lock.
 int rwlock_rd_lock(RWLock *lock) {
     int err;
-    if ((err = pthread_mutex_lock(&lock->mutex)) != 0) {
-        syserr("", err);
-    }
+    if ((err = pthread_mutex_lock(&lock->mutex)) != 0) syserr("", err);
 
     ++lock->wait_rd;
     if (lock->wait_wr > 0 || lock->work_wr > 0) { // TODO: check
         // reader should wait
         do {
-            if ((err = pthread_cond_wait(&lock->to_read, &lock->mutex)) != 0) {
+            if ((err = pthread_cond_wait(&lock->to_read, &lock->mutex)) != 0)
                 syserr("", err);
-            }
         } while (lock->work_wr > 0);
     }
     --lock->wait_rd;
@@ -68,19 +59,15 @@ int rwlock_rd_lock(RWLock *lock) {
 // Release read lock.
 int rwlock_rd_unlock(RWLock *lock) {
     int err;
-    if ((err = pthread_mutex_lock(&lock->mutex)) != 0) {
-        syserr("", err);
-    }
+    if ((err = pthread_mutex_lock(&lock->mutex)) != 0) syserr("", err);
 
     --lock->work_rd;
     if (lock->work_rd == 0 && lock->wait_wr > 0) {
-        if ((err = pthread_cond_signal(&lock->to_write)) != 0) {
+        if ((err = pthread_cond_signal(&lock->to_write)) != 0)
             syserr("", err);
-        }
     } else if (lock->work_rd == 0 && lock->wait_rd > 0) {
-        if ((err = pthread_cond_broadcast(&lock->to_read)) != 0) {
+        if ((err = pthread_cond_broadcast(&lock->to_read)) != 0)
             syserr("", err);
-        }
     }
     return pthread_mutex_unlock(&lock->mutex);
 }
@@ -88,16 +75,13 @@ int rwlock_rd_unlock(RWLock *lock) {
 // Acquire write lock.
 int rwlock_wr_lock(RWLock *lock) {
     int err;
-    if ((err = pthread_mutex_lock(&lock->mutex)) != 0) {
-        syserr("", err);
-    }
+    if ((err = pthread_mutex_lock(&lock->mutex)) != 0) syserr("", err);
 
     ++lock->wait_wr;
     while (lock->work_rd > 0 || lock->work_wr > 0) {
         // writer should wait
-        if ((err = pthread_cond_wait(&lock->to_write, &lock->mutex)) != 0) {
+        if ((err = pthread_cond_wait(&lock->to_write, &lock->mutex)) != 0)
             syserr("", err);
-        }
     }
     --lock->wait_wr;
     ++lock->work_wr;
@@ -107,20 +91,16 @@ int rwlock_wr_lock(RWLock *lock) {
 // Release write lock.
 int rwlock_wr_unlock(RWLock *lock) {
     int err;
-    if ((err = pthread_mutex_lock(&lock->mutex)) != 0) {
-        syserr("", err);
-    }
+    if ((err = pthread_mutex_lock(&lock->mutex)) != 0) syserr("", err);
 
     --lock->work_wr;
     // always at most only one writer working
     if (lock->wait_rd > 0) {
-        if ((err = pthread_cond_broadcast(&lock->to_read)) != 0) {
+        if ((err = pthread_cond_broadcast(&lock->to_read)) != 0)
             syserr("", err);
-        }
     } else if (lock->wait_wr > 0) {
-        if ((err = pthread_cond_signal(&lock->to_write)) != 0) {
+        if ((err = pthread_cond_signal(&lock->to_write)) != 0)
             syserr("", err);
-        }
     }
     return pthread_mutex_unlock(&lock->mutex);
 }
@@ -128,15 +108,9 @@ int rwlock_wr_unlock(RWLock *lock) {
 int rwlock_free(RWLock *lock) {
     int err;
 
-    if ((err = pthread_cond_destroy(&lock->to_read)) != 0) {
-        syserr("", err);
-    }
-    if ((err = pthread_cond_destroy(&lock->to_write)) != 0) {
-        syserr("", err);
-    }
-    if ((err = pthread_mutex_destroy(&lock->mutex)) != 0) {
-        syserr("", err);
-    }
+    if ((err = pthread_cond_destroy(&lock->to_read)) != 0) syserr("", err);
+    if ((err = pthread_cond_destroy(&lock->to_write)) != 0) syserr("", err);
+    if ((err = pthread_mutex_destroy(&lock->mutex)) != 0) syserr("", err);
     free(lock);
     return 0;
 }
